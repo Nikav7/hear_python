dbe0cc7ba933180acdf049b15553f6f3a51a9bfd
Nikav7 <112890224+Nikav7@users.noreply.github.com>
2024-07-24 15:14:16 +0200

    Update real_data.py
    set correct montage 10-20

--- src/real_data.py
+++ src/real_data.py
@@ -2,6 +2,7 @@
 import os
 import glob
 import scipy.io as sio
+from scipy.signal import lfilter, filtfilt, savgol_filter, triang
 import numpy as np
 import mne
 #from mne.viz import plot_topomap
@@ -37,23 +38,101 @@ for file in edf_files:
 raw_combined = mne.concatenate_raws(raw_list)
 print(raw_combined.info['chs'])
 
-raw_combined.drop_channels('Iz..')
+# PREPARE TEST DATA
 
 test_data = raw_combined.get_data()
+print(test_data.shape)
 
 # Save the combined Raw object to a new .fif file (optional)
 #raw_combined.save('combined_raw.fif', overwrite=True)
 
 
-## TRAIN AND APPLY NEW MODEL
+## PREPARE TRAIN DATA
+
 raw_1 = mne.io.read_raw_edf(f'subj_9/train/S009R01.edf', preload=True)
 raw_2 = mne.io.read_raw_edf(f'subj_9/train/S009R02.edf', preload=True)
 
 raw_list_= [raw_1, raw_2]
 
 print(raw_list_)
+
 # Concatenate all Raw objects
 train = mne.concatenate_raws(raw_list_)
+
+## SET MONTAGE TO GET ELECTRODES LOCATIONS
+
+std_montage = mne.channels.make_standard_montage("standard_1020")
+print(std_montage)
+new_names = {
+    'Fc5.': 'FC5',
+    'Fc3.': 'FC3',
+    'Fc1.': 'FC1',
+    'Fcz.': 'FCz',
+    'Fc2.': 'FC2',
+    'Fc4.': 'FC4',
+    'Fc6.': 'FC6',
+    'C5..': 'C5',
+    'C3..': 'C3',
+    'C1..': 'C1',
+    'Cz..': 'Cz',
+    'C2..': 'C2',
+    'C4..': 'C4',
+    'C6..': 'C6',
+    'Cp5.': 'CP5',
+    'Cp3.': 'CP3',
+    'Cp1.': 'CP1',
+    'Cpz.': 'CPz',
+    'Cp2.': 'CP2',
+    'Cp4.': 'CP4',
+    'Cp6.': 'CP6',
+    'Fp1.': 'Fp1',
+    'Fpz.': 'Fpz',
+    'Fp2.': 'Fp2',
+    'Af7.': 'AF7',
+    'Af3.': 'AF3',
+    'Afz.': 'AFz',
+    'Af4.': 'AF4',
+    'Af8.': 'AF8',
+    'F7..': 'F7',
+    'F5..': 'F5',
+    'F3..': 'F3',
+    'F1..': 'F1',
+    'Fz..': 'Fz',
+    'F2..': 'F2',
+    'F4..': 'F4',
+    'F6..': 'F6',
+    'F8..': 'F8',
+    'Ft7.': 'FT7',
+    'Ft8.': 'FT8',
+    'T7..': 'T7',
+    'T8..': 'T8',
+    'T9..': 'T9',
+    'T10.': 'T10',
+    'Tp7.': 'TP7',
+    'Tp8.': 'TP8',
+    'P7..': 'P7',
+    'P5..': 'P5',
+    'P3..': 'P3',
+    'P1..': 'P1',
+    'Pz..': 'Pz',
+    'P2..': 'P2',
+    'P4..': 'P4',
+    'P6..': 'P6',
+    'P8..': 'P8',
+    'Po7.': 'PO7',
+    'Po3.': 'PO3',
+    'Poz.': 'POz',
+    'Po4.': 'PO4',
+    'Po8.': 'PO8',
+    'O1..': 'O1',
+    'Oz..': 'Oz',
+    'O2..': 'O2',
+    'Iz..': 'Iz'
+}
+
+mne.rename_channels(train.info, new_names, allow_duplicates=False)
+
+train.set_montage(std_montage, on_missing='ignore')
 
 ## COMPUTE INTERPOLATION MATRIX OF RELATIVE DISTANCES D
 
@@ -92,13 +171,14 @@ def calculate_interpolation_matrix(chanlocs, k):
 
 #print(train.info['chs'])
 
-D = calculate_interpolation_matrix(train.info['chs'], 4)
+D = calculate_interpolation_matrix(raw_1.info['chs'], 4)
 np.set_printoptions(precision=3, floatmode="fixed")
 print(D)
 
 ## DEFINE AND TRAIN HEAR
 
 train_data = train.get_data()
+print(train_data.shape)
 
 fs = 160
 hear_mdl = HEAR(fs, D)
@@ -112,7 +192,7 @@ print('HEAR successfully trained.')
 
 ## APPLY HEAR
 
-with open('hear_model_real_ca.pkl', 'rb') as f:
+with open('hear_model_r.pkl', 'rb') as f:
     hear_mdl = pickle.load(f)
     output = hear_mdl.apply(test_data)
     print(output)
@@ -213,8 +293,8 @@ long_df = epochs.to_data_frame(time_format=None, index="condition", long_format=
 print(long_df)
 
 plt.figure()
-channels = ["Fc5.", "Fc6."]
-data = long_df.loc["T2"].query("channel in @channels")
+channels = ["C1..", "Cz.."]
+data = long_df.loc["T1"].query("channel in @channels")
 # convert channel column (CategoryDtype → string; for a nicer-looking legend)
 data["channel"] = data["channel"].astype(str)
 data.reset_index(drop=True, inplace=True)  # speeds things up
